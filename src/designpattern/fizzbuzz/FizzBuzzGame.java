5eadd4defee78123b5f659154b2ccbc57d2598eb
tomoharu ishibashi <bassyMac@tomMac.local>
2016-08-06 11:37:21 +0900

    ループカウントを50に変更

--- src/designpattern/fizzbuzz/FizzBuzzGame.java
+++ src/designpattern/fizzbuzz/FizzBuzzGame.java
@@ -5,7 +5,7 @@ import designpattern.fizzbuzz.strategy.Strategy;
 
 public class FizzBuzzGame {
 	public void playFizzBuzz() {
-		for (int i = 1; i < 40; i++) {
+		for (int i = 1; i < 50; i++) {
 			Strategy st = StrategyFactory.getStrategy(i);
 			st.call();
 		}
